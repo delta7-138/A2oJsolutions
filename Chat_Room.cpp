2e70677d972c5445722c27853d39d4931bd187e4
delta7-138 <krishna.sharma@iiitb.org>
2021-01-11 18:38:56 +0530

    nice problem

--- Chat_Room.cpp
+++ Chat_Room.cpp
@@ -1,24 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
-bool inString(string inp , char c){
-    for(int i = 0; i<inp.length; i++){
-        if(c==inp[i]){
-            return true;
+bool solve(string inp){
+    string refstr = "hello";
+    int ind = 0;
+    for(int i = 0; i<inp.length(); i++){
+        if(refstr[ind]==inp[i]){
+            ind++;
         }
+    }
+    if(ind==5){
+        return true;
     }
     return false;
 }
-
-bool solve(string inp){
-    int ind = 0;
-    string refstr = "hello";
-    for(int i = 0; i<inp.length(); i++){
-        if(inString(refstr , inp[i])){
-             
-        }
-    }
-}
-
 int main(){
     string inp;
     cin>>inp;
